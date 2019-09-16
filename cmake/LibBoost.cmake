ff3dfec64b3904e43a73cae19b7ad0fc8da5e11f
alexsanderlima <alexsanderlimare@gmail.com>
2019-09-16 00:50:29 -0300

    Update boost download script

--- cmake/LibBoost.cmake
+++ cmake/LibBoost.cmake
@@ -1,21 +1,18 @@
+message("VS version: ${MSVC_VERSION}")
+message("${boost_toolset}")
+
+if(MSVC_VERSION EQUAL 1916) #VS2017
+    set(BOOST_TOOLSET "msvc-14.1")
+endif()
+
 # Download and unpack lib boost at configure time
 configure_file(${PROJECT_SOURCE_DIR}/cmake/LibBoost.cmake.in ${PROJECT_SOURCE_DIR}/third_party/boost/CMakeLists.txt @ONLY)
 
 execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" -S . -B build
-	RESULT_VARIABLE result
-	WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/third_party/boost/
+        RESULT_VARIABLE result
+        WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/third_party/boost/
 )
 
 if(result)
-	message(FATAL_ERROR "CMake step for libboost failed: ${result}")
-endif()
-
-execute_process(
-	COMMAND ${CMAKE_COMMAND} --build build
-	RESULT_VARIABLE result
-	WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/third_party/boost/
-)
-
-if(result)
-	message(FATAL_ERROR "Build step for libboost failed: ${result}")
+        message(FATAL_ERROR "CMake step for libboost failed: ${result}")
 endif()
